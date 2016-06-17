1bd0cbc276b3ab3d601c329e97671ca344529bc4
施呵呵 <shirui816@gmail.com>
2016-06-17 17:31:50 -0500

    Update ubint_ho_devel.py

--- ubint_ho_devel.py
+++ ubint_ho_devel.py
@@ -6,19 +6,42 @@ import numpy
 from pandas import DataFrame as df
 from scipy.stats import kstat, moment
 from scipy.integrate import simps
+import argparse
+from argparse import RawTextHelpFormatter
+description="""
+An Umbrella Integration program.
+Written by Shirui shirui816@gmail.com
+
+### metafile format:
+/window/data window_center sprint_konst [Temperature]
+
+### window data file format:
+time_step coordinate (1-dimentional)
+"""
+parser = argparse.ArgumentParser(description=description,formatter_class=RawTextHelpFormatter)
+parser.add_argument('-P','--period',type=float, help='Optional, nonzero val if periodic, 0 for non-periodic system, default is 0', default=0, metavar='val', dest='peri')
+parser.add_argument('metafile', nargs=None, help='Meta file name') # nargs = 1 for a list
+parser.add_argument('max_bin', nargs=None, help='How many bins were used in integration', type=int)
+parser.add_argument('-T','--temperature', metavar='Temperature', dest='T', default=-1, type=float, help="Optional, set a default temperature globally")
+parser.add_argument('-o', '--order', metavar='order', dest='O', default=4, type=int, help="The order of A, default 4")
+args = parser.parse_args()
+alvars = vars(args)
 
 # Variables
 
-T = 300
-kbT = kb * T * NA / 1000
-MAX_BINS = 200 # how many bins were used in integration
+Peri = alvars['peri']
+O = alvars['O']
+T = alvars['T']
+kbT = 'nil'
+if T != -1:
+    kbT = kb * T * NA / 1000
+MAX_BINS = alvars['max_bin'] # how many bins were used in integration
 
+class NoTemperatureError(Exception):
+    pass
 
-### Read metafile
-## metafile format:
-# /path/to/file/of/reaction/coordinates/of/a/window window_center sprint_konst [Temperature]
-## coordinates file format:
-# time_step coordinate (1-dimentional)
+def pbc(r, d):
+    return(r-d*round(r/d))
 
 def loadmeta(filename):
     res = []
@@ -34,11 +57,13 @@ def loadmeta(filename):
                 d['kbT'] = float(t[3]) * kb * NA / 1000
             except:
                 d['kbT'] = kbT
+                if kbT == 'nil':
+                    raise NoTemperatureError("Sorry, no temperature was given by '-T' option nor the meta file!")
         res.append(d)
     return df(res)
 
 
-conf = loadmeta(argv[1])
+conf = loadmeta(alvars['metafile'])
 conf.sort_values(by='bin')
 
 mconf, Mconf = conf['bin'].min(), conf['bin'].max()
@@ -51,8 +76,21 @@ def a_u4(x, xbin, k1, k2, k3, k4, G2, K, kbT): # $\frac{\partial{A^u}}{\partial{
 def a_u3(x, xbin, k1, k2, k3, k4, G2, K, kbT):
     return(kbT*(x - k1)/k2+kbT * k3/(2*k2**2) * (1-(x-k1)**2/k2)-K*(x-xbin))
 
-def a_u2(x, xbin, k1, k2, k3, k4, G2, K, kbT): # Gives the same value as ubint.py
+def a_u2(x, xbin, k1, k2, k3, k4, G2, K, kbT):
     return(kbT*(x - k1)/k2-K*(x-xbin))
+
+def a_u4_pbc(x, xbin, k1, k2, k3, k4, G2, K, kbT): # $\frac{\partial{A^u}}{\partial{\xi}$
+    pbcx = pbc(x-xbin,Peri) + xbin
+    return(kbT*(pbcx - k1)/k2 + kbT * k3/(2*k2**2) * (1-(pbcx-k1)**2/k2) + kbT*(k3**2/(2*k2**5)-G2/(6*k2**2))*(pbcx-k1)**3-K*(pbcx-xbin))
+
+def a_u3_pbc(x, xbin, k1, k2, k3, k4, G2, K, kbT):
+    pbcx = pbc(x-xbin,Peri) + xbin
+    return(kbT*(pbcx - k1)/k2+kbT * k3/(2*k2**2) * (1-(pbcx-k1)**2/k2)-K*(pbcx-xbin))
+
+def a_u2_pbc(x, xbin, k1, k2, k3, k4, G2, K, kbT):
+    pbcx = pbc(x-xbin,Peri) + xbin
+    return(kbT*(pbcx - k1)/k2-K*(pbcx-xbin))
+
 
 
 from math import sqrt, pi
@@ -79,7 +117,8 @@ def P2(xi, t, a1, a2, a3, a4, NF):
     EXP = exp(-1/t * (a1*(xi)+a2*(xi)**2))
     return EXP/NF
 
-INT_PATH = linspace((mconf* 2-2 * Mconf), (2*Mconf-mconf), MAX_BINS * 4)
+
+INT_PATH = linspace((mconf* 2-2 * Mconf), (2*Mconf-mconf), MAX_BINS * 4) # Enlarge the integration zone
 
 para = []
 def get_para():
@@ -93,19 +132,35 @@ def get_para():
         a2 = t / (2 * k2)
         a3 = abs(-t * k3/(6 * k2 ** 3))
         a4 = abs(-G2 * t/(24* k2**2) + t *k3 ** 2/(8 * k2 ** 5))
-        NF = simps(EP4(INT_PATH, t, a1, a2, a3, a4), INT_PATH) # Normalization factor of P_i
+        eepp = eval("EP%s" % (O))
+        NF = simps(eepp(INT_PATH, t, a1, a2, a3, a4), INT_PATH) # Normalization factor of P_i
         NF = nan_to_num(NF)
-        #print(NF, xibin)
+        print(NF, xibin)
         para.append((xibin, a1, a2, a3, a4, k1, k2, k3, k4, G2, K, t, NF))
         ############# 0      1   2   3   4   5   6   7   8   9  10 11 12
 get_para() # this is the most time-consuming part, reading all coordiante files.
 
+PP = eval("P%s" % (O))
+
 def Pi(xi, p, paras):
-    ai = P4(xi, p[11], p[1], p[2], p[3], p[4], p[12])
+    ai = PP(xi, p[11], p[1], p[2], p[3], p[4], p[12])
     alsum = 0
     for pa in paras:
-        alsum += P4(xi, pa[11], pa[1], pa[2], pa[3], pa[4], pa[12])
+        alsum += PP(xi, pa[11], pa[1], pa[2], pa[3], pa[4], pa[12])
     return ai / alsum
+
+def Pi_pbc(xi, p, paras):
+    ai = PP(xi, p[11], p[1], p[2], p[3], p[4], p[12])
+    alsum = 0
+    for pa in paras:
+        pbcxi = pbc(xi- pa[0], Peri)+pa[0]
+        alsum += PP(xi, pa[11], pa[1], pa[2], pa[3], pa[4], pa[12])
+    return ai / alsum
+
+if not Peri == 0:
+    Pi = Pi_pbc
+
+AU = eval('a_u%s%s' % (O, '_pbc' if Peri else ''))
 
 dau_dxis = [] # $\frac{\partial{A^u}}{\partial{\xi}}$
 
@@ -114,7 +169,8 @@ def get_dau_dxis():
     for xi in xis:
         dau_dxi = 0
         for p in para:
-            dau_dxi += a_u4(xi, p[0], p[5], p[6], p[7], p[8], p[9], p[10], p[11]) * Pi(xi, p, para)
+            dau_dxi += AU(xi, p[0], p[5], p[6], p[7], p[8], p[9], p[10], p[11]) * Pi(xi, p, para)
+            #dau_dxi += aa_u4(xi, p[0], p[1], p[2], p[3], p[4], p[5], p[10]) * Pi(xi, p, para) # Same with above
         dau_dxis.append(dau_dxi)
 get_dau_dxis()
 
@@ -124,12 +180,12 @@ PMF = []
 for r in xis:
     PMF.append(simps(dau_dxis[xis<=r], xis[xis<=r]))
 
-o = open('free_ho_py.txt','w')
+o = open("%sth_order_free_energy.txt" % (O),'w')
 for i,j in zip(xis, PMF):
     o.write('%s %s\n' % (i,j))
 o.close()
 
-o = open('mean_force_ho_py.txt', 'w')
+o = open("%sth_order_mean_force.txt" % (O), 'w')
 for i, j in zip(xis, dau_dxis):
 	o.write("%s %s\n" % (i, j))
 o.close()
