8a8de8e8b065218756f3864b2c22bafbb59d8c8e
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2020-01-14 02:36:59 +0800

    Update ubint_kde.py

--- ubint_kde.py
+++ ubint_kde.py
@@ -13,12 +13,14 @@ from argparse import RawTextHelpFormatter
 from scipy.integrate import simps
 from scipy.stats import gaussian_kde
 from scipy.stats import circmean
+from scipy.stats import kstat
 
 # use gaussian_kde and fit exp(-(\sum_i a_i x_i^(i-1)); periodic data
 # the numerical stability of exp(-(\sum_i a_i x_i^(i-1)) is the key, make sure
 # that fitting range is large enough so that mean force does not diverge.
 
 description = """An Umbrella Integration program (Gaussian KDE version).
+Periodic function is not fully tested!
 Written by Shirui shirui816@gmail.com
 ### metafile format:
 /window/data window_center sprint_konst [Temperature]
@@ -32,6 +34,11 @@ arg_parser.add_argument('-P', '--period',
                         default=0, metavar='val', dest='period', type=float,
                         help='Optional, nonzero val if periodic, 0 for '
                              'non-periodic system, default is 0.')
+arg_parser.add_argument('-M', '--mode',
+                        default="kde", metavar='kde|kastner', dest='mode', type=str,
+                        help='Optional, method to estimate the distribution. '
+                             'Defualt is Gaussian KDE method.',
+                        choices=['kde', 'kastner'])
 arg_parser.add_argument('-o', '--output',
                         metavar='Output free energy file',
                         default='free_py.txt', dest='out_put',
@@ -79,14 +86,18 @@ _out_put_file.write('#r PMF MF\n')
 _out_put_file.close()
 _out_put_file = open(alvars['out_put'], 'a')
 _order = alvars['order']
+_mode = alvars['mode']
 
 _kb = _is_reduced or KB * NA
 
 if _period > 0:
     _xi_range[0] = -_period / 2
     _xi_range[1] = _period / 2
-    print("Peroid is set, the data range is set to (-%.3f/2, %.3f/2]!"
-          % (_period, _period))
+    print("Peroid is set to %.2f, the data range is set to (-%.2f, %.2f]!"
+          % (_period, _period / 2, _period / 2))
+
+if _order > 4 and _mode == 'kastner':
+    raise ValueError("The order of kastner method is up to 4!")
 
 _meta_file = open(alvars['meta_file'], 'r')
 _min = []
@@ -123,9 +134,11 @@ for _line in _meta_file:
     if _period > 0:
         _xi_mean_w = circmean(_window_data, high=_period / 2, low=-_period / 2)
         _window_data = pbc(_window_data - _xi_mean_w, _period)
-        # move the "center" of distribution to 0, currently for relatively
+        # move the MEAN of the distribution to 0, only for relatively
         # symmetric distributions. If the distribution is heavily skewed,
-        # use the circle-midpoint method to move data.
+        # the distribution should be "unwrapped" into a whole period and
+        # PBC of _delta_xis should be removed, i.e., distances further than
+        # half period is permitted. However, I don't think this case is physical...
         _xi_var_w = np.mean(_window_data ** 2)
         _delta_xis = pbc(_xis - _xi_mean_w, _period)
         _delta_xis_ref = pbc(_xis - _xi_center_w, _period)
@@ -143,18 +156,52 @@ for _line in _meta_file:
         # and summation of p^b_w
         _tmp = 1 / np.sqrt(2 * np.pi) * 1 / np.sqrt(_xi_var_w) * \
                np.exp(-0.5 * _delta_xis ** 2 / _xi_var_w)
+        _ntmp = 1
         _dAu_dxis_pb_w += (_kbT_w * _delta_xis / _xi_var_w -
                            _k_w * _delta_xis_ref) * _tmp
-        _pb_xi += _tmp
     else:
-        _z = np.polyfit(_x, -_kbT_w * np.log(_pi_w), _order, w=_pi_w)
-        _Z = np.poly1d(_z)
-        _dz = np.poly1d(_z[:-1] * np.arange(_order, 0, -1))
-        _tmp = np.exp(-_Z(_delta_xis) / _kbT_w)
-        _ntmp = np.sum(_tmp)
-        _pb_xi += _tmp / _ntmp
-        _dAu_dxis_pb_w += (_kbT_w * _dz(_delta_xis) -
-                           _k_w * _delta_xis_ref) * _tmp / _ntmp
+        if _mode == 'kde':
+            _z = np.polyfit(_x, -_kbT_w * np.log(_pi_w), _order, w=_pi_w)
+            _Z = np.poly1d(_z)
+            _dz = np.poly1d(_z[:-1] * np.arange(_order, 0, -1))
+            _tmp = np.exp(-_Z(_delta_xis) / _kbT_w)
+            _ntmp = np.sum(_tmp)  # normalization factor, simple summation.
+            _dAu_dxis_pb_w += (_kbT_w * _dz(_delta_xis) -
+                               _k_w * _delta_xis_ref) * _tmp / _ntmp
+        else:
+            m1 = _xi_mean_w
+            m2 = _xi_var_w
+            m3 = np.mean((_window_data) ** 3)
+            m4 = np.mean((_window_data) ** 4)
+            gamma1 = m3 / m2 ** 1.5
+            gamma2 = m4 / m2 ** 2 - 3
+            _a1_w = _kbT_w * (0.5 * m3 / m2 ** 2 - m1 / m2)
+            _a2_w = _kbT_w * (0.5 / m2)
+            _a3_w = _kbT_w * (-m3 / (6 * m2 ** 3))
+            _a4_w = np.abs(_kbT_w * (-gamma2 / (24 * m2 ** 2) + m3 ** 2 / (8 * m2 ** 5)))
+            _xi_k2_w = kstat(_window_data, n=2)
+            # data is unwrapped in the whole period with zero mean,
+            # for lightly skewed data.
+            _xi_k3_w = kstat(_window_data, n=3)
+            _xi_k4_w = kstat(_window_data, n=4)
+            _xi_g2_w = _xi_k4_w / _xi_k2_w ** 2
+            if _order == 3:
+                _tmp = np.exp(-(_a1_w * _delta_xis + _a2_w * _delta_xis ** 2 +
+                                _a3_w * _delta_xis ** 3) / _kbT_w)
+                _ntmp = np.sum(_tmp)
+                _dAu_dxis_pb_w += (_kbT_w * _delta_xis / _xi_k2_w +
+                                   0.5 * _kbT_w * _xi_k3_w / _xi_k2_w ** 2 *
+                                   (1 - _delta_xis ** 2 / _xi_k2_w) - _k_w * _delta_xis_ref) * _tmp / _ntmp
+            if _order == 4:
+                _tmp = np.exp(-(_a1_w * _delta_xis + _a2_w * _delta_xis ** 2 +
+                                _a3_w * _delta_xis ** 3 + _a4_w * _delta_xis ** 4) / _kbT_w)
+                _ntmp = np.sum(_tmp)
+                _dAu_dxis_pb_w += (_kbT_w * _delta_xis / _xi_k2_w + 0.5 * _kbT_w * _xi_k3_w / _xi_k2_w ** 2 *
+                             (1 - _delta_xis ** 2 / _xi_k2_w) +
+                             _kbT_w * (0.5 * _xi_k3_w ** 2 / _xi_k2_w ** 5 -
+                                       1 / 6 * _xi_g2_w / _xi_k2_w ** 2) *
+                             _delta_xis ** 3 - _k_w * _delta_xis_ref) * _tmp / _ntmp
+    _pb_xi += _tmp / _ntmp
     _min.append(_window_data.min() + _xi_mean_w)
     _min.append(_window_data.max() + _xi_mean_w)
 
