326b5b23962b73b1496c98b253218b2a401b3021
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2020-01-15 17:40:22 +0800

    Update ubint.py

--- ubint.py
+++ ubint.py
@@ -20,7 +20,6 @@ from scipy.stats import kstat
 # that fitting range is large enough so that mean force does not diverge.
 
 description = """An Umbrella Integration program (Gaussian KDE version).
-Periodic function is not fully tested!
 Written by Shirui shirui816@gmail.com
 ### metafile format:
 /window/data window_center sprint_konst [Temperature]
@@ -55,14 +54,11 @@ arg_parser.add_argument('-Q', '--order',
                         default=2, metavar='order',
                         dest='order', type=int,
                         help="Order of probability function.")
-arg_parser.add_argument('meta_file',
-                        nargs=None,
-                        help='Meta file name')
+arg_parser.add_argument('meta_file', help='Meta file name')
 arg_parser.add_argument('range', nargs=2, default=None,
                         metavar='Range of xi', type=float,
                         help="Range of reaction coordinate.")
-arg_parser.add_argument('max_bin',
-                        type=int, nargs=None,
+arg_parser.add_argument('max_bin', type=int,
                         help='How many bins were used in integration.')
 
 args = arg_parser.parse_args()
@@ -76,36 +72,36 @@ def pbc(x, d):
 
 
 # Variables
-_period = alvars['period']
-_temperature = alvars['temperature']
-_is_reduced = alvars['is_reduced']
-_max_bin = alvars['max_bin']  # how many bins were used in integration
-_xi_range = alvars['range']
-_out_put_file = open(alvars['out_put'], 'w')
-_out_put_file.write('#r PMF MF\n')
-_out_put_file.close()
-_out_put_file = open(alvars['out_put'], 'a')
-_order = alvars['order']
-_mode = alvars['mode']
+period = alvars['period']
+temperature = alvars['temperature']
+is_reduced = alvars['is_reduced']
+max_bin = alvars['max_bin']  # how many bins were used in integration
+xi_range = alvars['range']
+out_put_file = open(alvars['out_put'], 'w')
+out_put_file.write('#r PMF MF\n')
+out_put_file.close()
+out_put_file = open(alvars['out_put'], 'a')
+order = alvars['order']
+mode = alvars['mode']
+meta_file = open(alvars['meta_file'], 'r')
 
-_kb = _is_reduced or KB * NA
+kb = is_reduced or KB * NA
 
-if _period > 0:
-    _xi_range[0] = -_period / 2
-    _xi_range[1] = _period / 2
+if period > 0:
+    xi_range[0] = -period / 2
+    xi_range[1] = period / 2
     print("Peroid is set to %.2f, the data range is set to (-%.2f, %.2f]!"
-          % (_period, _period / 2, _period / 2))
+          % (period, period / 2, period / 2))
 
-if _order > 4 and _mode == 'kastner':
+if order > 4 and mode == 'kastner':
     raise ValueError("The order of kastner method is up to 4!")
 
-_meta_file = open(alvars['meta_file'], 'r')
-_min = []
-_d = _xi_range[1] - _xi_range[0]
-_x = np.linspace(-_d, _d, _max_bin * 2)
-_xis = np.linspace(_xi_range[0], _xi_range[1], _max_bin)
-_dAu_dxis_pb_w = np.zeros((_max_bin,))
-_pb_xi = np.zeros((_max_bin,))
+min_ = []
+d = xi_range[1] - xi_range[0]
+x = np.linspace(-d, d, max_bin * 2)
+xis = np.linspace(xi_range[0], xi_range[1], max_bin)
+dAu_dxis_pb_w = np.zeros((max_bin,))
+pb_xi = np.zeros((max_bin,))
 
 
 class NoTemperatureError(Exception):
@@ -118,102 +114,105 @@ def pbc(x, d):
     return x - d * np.round(x / d)
 
 
-for _line in _meta_file:
+for line in meta_file:
     # loop for each window.
-    if not re.search('^#', _line) is None:
+    if not re.search('^#', line) is None:
         continue
-    _line = re.split('\s+', _line.strip())
-    if _temperature == -1 and len(_line) != 4:
+    line = re.split('\s+', line.strip())
+    if temperature == -1 and len(line) != 4:
         raise NoTemperatureError("You have not set temperature for this "
                                  "window or a global temperature!")
-    _window_data = pd.read_csv(_line[0], header=None, squeeze=1,
-                               delim_whitespace=True, comment='#').values[:, 1]
-    _xi_center_w = float(_line[1])
-    _k_w = float(_line[2])
-    _kbT_w = float(_line[3]) if len(_line) == 4 else _kb * _temperature
-    if _period > 0:
-        _xi_mean_w = circmean(_window_data, high=_period / 2, low=-_period / 2)
-        _window_data = pbc(_window_data - _xi_mean_w, _period)
+    window_data = pd.read_csv(line[0], header=None, squeeze=1,
+                              delim_whitespace=True, comment='#').values[:, 1]
+    xi_center_w = float(line[1])
+    k_w = float(line[2])
+    kbT_w = float(line[3]) if len(line) == 4 else kb * temperature
+    if period > 0:
+        xi_mean_w = circmean(window_data, high=period / 2, low=-period / 2)
+        window_data = pbc(window_data - xi_mean_w, period)
         # move the MEAN of the distribution to 0, only for relatively
         # symmetric distributions. If the distribution is heavily skewed,
         # the distribution should be "unwrapped" into a whole period and
         # PBC of _delta_xis should be removed, i.e., distances further than
         # half period is permitted. However, I don't think this case is physical...
-        _xi_var_w = np.mean(_window_data ** 2)
-        _delta_xis = pbc(_xis - _xi_mean_w, _period)
-        _delta_xis_ref = pbc(_xis - _xi_center_w, _period)
+        xi_var_w = np.mean(window_data ** 2)
+        delta_xis = pbc(xis - xi_mean_w, period)
+        delta_xis_ref = pbc(xis - xi_center_w, period)
     else:
-        _xi_mean_w = _window_data.mean()
-        _xi_var_w = _window_data.var()
-        _window_data = _window_data - _xi_mean_w
-        _delta_xis = _xis - _xi_mean_w
-        _delta_xis_ref = _xis - _xi_center_w
-    _kde = gaussian_kde(_window_data)
-    _pi_w = _kde(_x)
+        xi_mean_w = window_data.mean()
+        xi_var_w = window_data.var()
+        window_data = window_data - xi_mean_w
+        delta_xis = xis - xi_mean_w
+        delta_xis_ref = xis - xi_center_w
+    kde = gaussian_kde(window_data)
+    pi_w = kde(x)
 
-    if _order == 2:
+    if order == 2:
         # evaluate \partial A^{ub}_w / \partial \xi \times P^b_w(\xi)
         # and summation of p^b_w
-        _tmp = 1 / np.sqrt(2 * np.pi) * 1 / np.sqrt(_xi_var_w) * \
-               np.exp(-0.5 * _delta_xis ** 2 / _xi_var_w)
-        _ntmp = 1
-        _dAu_dxis_pb_w += (_kbT_w * _delta_xis / _xi_var_w -
-                           _k_w * _delta_xis_ref) * _tmp
+        tmp = 1 / np.sqrt(2 * np.pi) * 1 / np.sqrt(xi_var_w) * \
+              np.exp(-0.5 * delta_xis ** 2 / xi_var_w)
+        n_tmp = 1
+        dAu_dxis_pb_w += (kbT_w * delta_xis / xi_var_w -
+                          k_w * delta_xis_ref) * tmp
     else:
-        if _mode == 'kde':
-            _z = np.polyfit(_x, -_kbT_w * np.log(_pi_w), _order, w=_pi_w)
-            _Z = np.poly1d(_z)
-            _dz = np.poly1d(_z[:-1] * np.arange(_order, 0, -1))
-            _tmp = np.exp(-_Z(_delta_xis) / _kbT_w)
-            _ntmp = np.sum(_tmp)  # normalization factor, simple summation.
-            _dAu_dxis_pb_w += (_kbT_w * _dz(_delta_xis) -
-                               _k_w * _delta_xis_ref) * _tmp / _ntmp
+        if mode == 'kde':
+            z_ = np.polyfit(x, -kbT_w * np.log(pi_w), order, w=pi_w)
+            # weights are set to be the probability itself, the fitting is
+            # in well accord within the data range. PDF out of the data range
+            # extended by Gaussian KDE is very close to 0.
+            Z_ = np.poly1d(z_)
+            dz_ = np.poly1d(z_[:-1] * np.arange(order, 0, -1))
+            tmp = np.exp(-Z_(delta_xis) / kbT_w)
+            n_tmp = np.sum(tmp)  # normalization factor, simple summation.
+            dAu_dxis_pb_w += (kbT_w * dz_(delta_xis) -
+                              k_w * delta_xis_ref) * tmp / n_tmp
         else:
-            m1 = _xi_mean_w
-            m2 = _xi_var_w
-            m3 = np.mean((_window_data) ** 3)
-            m4 = np.mean((_window_data) ** 4)
+            m1 = xi_mean_w
+            m2 = xi_var_w
+            m3 = np.mean(window_data ** 3)
+            m4 = np.mean(window_data ** 4)
             gamma1 = m3 / m2 ** 1.5
             gamma2 = m4 / m2 ** 2 - 3
-            _a1_w = _kbT_w * (0.5 * m3 / m2 ** 2 - m1 / m2)
-            _a2_w = _kbT_w * (0.5 / m2)
-            _a3_w = _kbT_w * (-m3 / (6 * m2 ** 3))
-            _a4_w = np.abs(_kbT_w * (-gamma2 / (24 * m2 ** 2) + m3 ** 2 / (8 * m2 ** 5)))
-            _xi_k2_w = kstat(_window_data, n=2)
+            a1_w = kbT_w * (0.5 * m3 / m2 ** 2 - m1 / m2)
+            a2_w = kbT_w * (0.5 / m2)
+            a3_w = kbT_w * (-m3 / (6 * m2 ** 3))
+            a4_w = np.abs(kbT_w * (-gamma2 / (24 * m2 ** 2) + m3 ** 2 / (8 * m2 ** 5)))
+            xi_k2_w = kstat(window_data, n=2)
             # data is unwrapped in the whole period with zero mean,
             # for lightly skewed data.
-            _xi_k3_w = kstat(_window_data, n=3)
-            _xi_k4_w = kstat(_window_data, n=4)
-            _xi_g2_w = _xi_k4_w / _xi_k2_w ** 2
-            if _order == 3:
-                _tmp = np.exp(-(_a1_w * _delta_xis + _a2_w * _delta_xis ** 2 +
-                                _a3_w * _delta_xis ** 3) / _kbT_w)
-                _ntmp = np.sum(_tmp)
-                _dAu_dxis_pb_w += (_kbT_w * _delta_xis / _xi_k2_w +
-                                   0.5 * _kbT_w * _xi_k3_w / _xi_k2_w ** 2 *
-                                   (1 - _delta_xis ** 2 / _xi_k2_w) - _k_w * _delta_xis_ref) * _tmp / _ntmp
-            if _order == 4:
-                _tmp = np.exp(-(_a1_w * _delta_xis + _a2_w * _delta_xis ** 2 +
-                                _a3_w * _delta_xis ** 3 + _a4_w * _delta_xis ** 4) / _kbT_w)
-                _ntmp = np.sum(_tmp)
-                _dAu_dxis_pb_w += (_kbT_w * _delta_xis / _xi_k2_w + 0.5 * _kbT_w * _xi_k3_w / _xi_k2_w ** 2 *
-                             (1 - _delta_xis ** 2 / _xi_k2_w) +
-                             _kbT_w * (0.5 * _xi_k3_w ** 2 / _xi_k2_w ** 5 -
-                                       1 / 6 * _xi_g2_w / _xi_k2_w ** 2) *
-                             _delta_xis ** 3 - _k_w * _delta_xis_ref) * _tmp / _ntmp
-    _pb_xi += _tmp / _ntmp
-    _min.append(_window_data.min() + _xi_mean_w)
-    _min.append(_window_data.max() + _xi_mean_w)
+            xi_k3_w = kstat(window_data, n=3)
+            xi_k4_w = kstat(window_data, n=4)
+            xi_g2_w = xi_k4_w / xi_k2_w ** 2
+            if order == 3:
+                tmp = np.exp(-(a1_w * delta_xis + a2_w * delta_xis ** 2 +
+                               a3_w * delta_xis ** 3) / kbT_w)
+                n_tmp = np.sum(tmp)
+                dAu_dxis_pb_w += (kbT_w * delta_xis / xi_k2_w +
+                                  0.5 * kbT_w * xi_k3_w / xi_k2_w ** 2 *
+                                  (1 - delta_xis ** 2 / xi_k2_w) - k_w * delta_xis_ref) * tmp / n_tmp
+            if order == 4:
+                tmp = np.exp(-(a1_w * delta_xis + a2_w * delta_xis ** 2 +
+                               a3_w * delta_xis ** 3 + a4_w * delta_xis ** 4) / kbT_w)
+                n_tmp = np.sum(tmp)
+                dAu_dxis_pb_w += (kbT_w * delta_xis / xi_k2_w + 0.5 * kbT_w * xi_k3_w / xi_k2_w ** 2 *
+                                  (1 - delta_xis ** 2 / xi_k2_w) +
+                                  kbT_w * (0.5 * xi_k3_w ** 2 / xi_k2_w ** 5 -
+                                           1 / 6 * xi_g2_w / xi_k2_w ** 2) *
+                                  delta_xis ** 3 - k_w * delta_xis_ref) * tmp / n_tmp
+    pb_xi += tmp / n_tmp
+    min_.append(window_data.min() + xi_mean_w)
+    min_.append(window_data.max() + xi_mean_w)
 
-if min(_min) > _xi_range[0] or max(_min) < _xi_range[1]:
+if min(min_) > xi_range[0] or max(min_) < xi_range[1]:
     warnings.warn("Warning, xi range exceeds the sample range!",
                   UserWarning)
 
-if _period != 0 and max(_min) - min(_min) < _period:
+if period != 0 and max(min_) - min(min_) < period:
     warnings.warn("Your sampled data is lesser than 1 period!",
                   UserWarning)
 
-_dAu_dxis = _dAu_dxis_pb_w / _pb_xi
-_pmf = np.array([simps(_dAu_dxis[_xis <= r], _xis[_xis <= r]) for r in _xis])
-np.savetxt(_out_put_file, np.vstack([_xis, _pmf, _dAu_dxis]).T, fmt="%.6f")
-_out_put_file.close()
+dAu_dxis = dAu_dxis_pb_w / pb_xi
+pmf = np.array([simps(dAu_dxis[xis <= r], xis[xis <= r]) for r in xis])
+np.savetxt(out_put_file, np.vstack([xis, pmf, dAu_dxis]).T, fmt="%.6f")
+out_put_file.close()
